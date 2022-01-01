baa16b8980362aea150e57db37156553f00db8c5
dhruval0 <dhruvaldhorajia2014@gmail.com>
2022-01-01 15:16:43 +0530

    Initializing CI/CD setup

--- setup.sh
+++ setup.sh
@@ -1,25 +1,14 @@
 #!/bin/sh
 
-# navigate into our working directory where we have all our github files
-/home/ec2-user/firstdeploy
-
 # Install pip
-# sudo apt install -y python3-pip
+sudo yum install -y python3-pip
 
-# # Install Node
-# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-# sudo apt install nodejs
+# Install Node
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo yum install nodejs
 sudo npm i pm2 -g
 
 # Install virtualenv
 pip install virtualenv
-# or sudo apt install python3.8-venv
+# or sudo yum install python3.8-venv
 python3 -m venv ~/env
-
-source env/bin/activate
-
-pip3 install -r requirements.txt
-
-cd FastAPI-with-MongoDB/app/
-
-pm2 start ecosystem.config.js
