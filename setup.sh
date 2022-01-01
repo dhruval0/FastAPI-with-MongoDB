657a68eb30fb724f0d1b0640db671e1ed97e43d0
dhruval0 <dhruvaldhorajia2014@gmail.com>
2022-01-01 12:27:45 +0530

    Added CI/CD setup

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# navigate into our working directory where we have all our github files
+/home/ec2-user/firstdeploy
+
+# Install pip
+# sudo apt install -y python3-pip
+
+# # Install Node
+# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+# sudo apt install nodejs
+sudo npm i pm2 -g
+
+# Install virtualenv
+pip install virtualenv
+# or sudo apt install python3.8-venv
+python3 -m venv ~/env
+
+source env/bin/activate
+
+cd FastAPI-with-MongoDB/app/
+
+pm2 start ecosystem.config.js
