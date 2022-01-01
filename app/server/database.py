a76415236667b8925bc085afc86592b069f38299
dhruval0 <dhruvaldhorajia2014@gmail.com>
2022-01-01 09:40:06 +0530

    Change address of mongodb

--- app/server/database.py
+++ app/server/database.py
@@ -1,7 +1,7 @@
 import motor.motor_asyncio
 from bson.objectid import ObjectId
 
-MONGO_DETAILS = "mongodb://localhost:27017"
+MONGO_DETAILS = "mongodb://http://18.222.138.190:27017"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 database = client.users
 user_collection = database.get_collection("users_collection")
