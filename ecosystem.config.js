b923b0682c8c270f33116adce09fc12ebfb45d28
Dhruval Dhorajia <dhruvaldhorajia2014@gmail.com>
2022-08-08 11:07:02 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: 'FastAPI-with-MongoDB',
-        script: "python3 main.py",
+        script: "python3 app/main.py",
         env: {
             // PORT: 80
         }
