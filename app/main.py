8f21e2a9ca650e18499a7eb610450112b3b46ffe
Dhruval Dhorajia <dhruvaldhorajia2014@gmail.com>
2022-08-08 18:13:01 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,4 +2,4 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host=127.0.0.1, port=8000, lifespan="on")
+    uvicorn.run("server.app:app", host="127.0.0.1", port=8000, lifespan="on")
